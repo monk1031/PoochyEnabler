7445843bfbb1bbf4ecf10fce0f2c36ca3b253eea
monk1031 <sasakiyuki0528@gmail.com>
2026-06-20 16:51:45 +0900

    constants, iohelper

---
+++ PoochyEnabler/Constants.cs
@@ -0,0 +1,18 @@
+﻿namespace PoochyEnabler
+{
+    public static class Constants
+    {
+        public const int HexBase = 16;
+        public const int BitsPerByte = 8;
+        public const int CharPerByte = 2;
+        public const int NibbleShift = 4;
+        public const int NibbleMask = 0xF;
+        public const int ByteMask = 0xFF;
+        public const int UShortMask = 0xFFFF;
+        public const uint UIntMask = 0xFFFFFFFFU;
+        public const uint AlignMask = 0xFFFFFFFCU;
+        public const uint BaseAddr = 0x8000000U;
+        public const int PaddingByte = 0x0;
+        public const int FreeSpaceByte = 0xFF;
+    }
+}
---
+++ PoochyEnabler/IOHelper.cs
@@ -0,0 +1,41 @@
+﻿namespace PoochyEnabler
+{
+    public static class IOHelper
+    {
+        public static uint ReadUShortLE(byte[] data, uint offset)
+        {
+            return (uint)(data[offset]
+                 | (data[offset + 1] << Constants.BitsPerByte));
+        }
+
+        public static uint ReadUIntLE(byte[] data, uint offset)
+        {
+            return (uint)data[offset]
+                 | ((uint)data[offset + 1] << Constants.BitsPerByte)
+                 | ((uint)data[offset + 2] << (Constants.BitsPerByte * 2))
+                 | ((uint)data[offset + 3] << (Constants.BitsPerByte * 3));
+        }
+
+        public static bool TryReadPointer(uint ptrOffset, byte[] data, out uint? resultOffset)
+        {
+            uint rawAddr = ReadUIntLE(data, ptrOffset);
+
+            // check null pointer
+            if (rawAddr == 0)
+            {
+                resultOffset = null;
+                return true;
+            }
+
+            // valid?
+            if (rawAddr < Constants.BaseAddr)
+            {
+                resultOffset = null;
+                return false;
+            }
+
+            resultOffset = rawAddr - Constants.BaseAddr;
+            return true;
+        }
+    }
+}
